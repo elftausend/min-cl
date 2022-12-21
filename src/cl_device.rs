f09b7aeb8d7b07e0131419f534c46a3912b20dee
elftausend <76885970+elftausend@users.noreply.github.com>
2022-12-21 20:07:41 +0100

    Add cldevice

---
+++ src/cl_device.rs
@@ -0,0 +1,34 @@
+use crate::{api::{CLIntDevice, Context, CommandQueue, DeviceType, get_device_ids, get_platforms, create_context, create_command_queue, OCLErrorKind}, Error};
+
+
+/// Internal representation of an OpenCL Device.
+#[derive(Debug)]
+pub struct CLDevice {
+    pub device: CLIntDevice,
+    pub ctx: Context,
+    pub queue: CommandQueue,
+    pub unified_mem: bool,
+}
+
+impl CLDevice {
+    pub fn new(device_idx: usize) -> Result<CLDevice, Error> {
+        let platform = get_platforms()?[0];
+        let devices = get_device_ids(platform, &(DeviceType::GPU as u64))?;
+
+        if device_idx >= devices.len() {
+            return Err(OCLErrorKind::InvalidDeviceIdx.into());
+        }
+        let device = devices[0];
+
+        let ctx = create_context(&[device])?;
+        let queue = create_command_queue(&ctx, device)?;
+        let unified_mem = device.unified_mem()?;
+
+        Ok(CLDevice {
+            device,
+            ctx,
+            queue,
+            unified_mem,
+        })
+    }
+}
